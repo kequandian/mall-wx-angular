59378b482383476918b0089aab8a34c67549674e
vincent@pc <vincent@jfeat.com>
2016-06-13 17:21:14 +0800

    gulpfiles.js

--- app/js/global.js
+++ app/js/global.js
@@ -4,7 +4,7 @@ angular.module('global', [])
     .constant('GlobalVariable', {
         // 服务器地址
         SERVER_PATH: 'http://112.74.26.228:10080/rest',
-        ACCESS_TOKEN: 'eyJ0b2tlbiI6ImZjOWYzZjFmOWQ3MWFjYmU5YmJkMjUxNzIxMzY1MTU5Nzc0NzYyOTMiLCJsb2dpbl9uYW1lIjoiamluZ2ZlaSJ9'
+        ACCESS_TOKEN: 'eyJ0b2tlbiI6ImU4Y2E2ODQ0OWY1M2JmZWFiZDFkMWEyMjhkMjI3YTBmNzNjN2UxN2EiLCJsb2dpbl9uYW1lIjoiamluZ2ZlaSJ9'
     })
 
     .value('LoginSession', {
