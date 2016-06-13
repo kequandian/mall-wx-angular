983b0b38a27ca2e697e793685cc757f12968b93a
vincent <vincent@kequandian.net>
2016-06-14 00:04:14 +0800

    withdraw rename

--- app/pages/seller/pages/with-draw/with-draw-route.js
+++ app/pages/seller/pages/with-draw/with-draw-route.js
@@ -6,7 +6,7 @@ angular.module('withdraw.route', ['withdraw.controller'])
     .config(['$stateProvider', function($stateProvider) {
         $stateProvider.state('withdraw', {
             url: '/withdraw',
-            templateUrl: 'pages/seller/pages/withdraw/withdraw.html',
+            templateUrl: '/withdraw.html',
             controller: 'WithdrawController'
         })
     }]);
