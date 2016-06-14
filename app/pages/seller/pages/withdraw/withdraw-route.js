bdc222aec3afaa55f26ded2b43aa098974977cef
vincent <vincent@kequandian.net>
2016-06-14 10:00:38 +0800

    withdraw route

--- app/pages/seller/pages/withdraw/withdraw-route.js
+++ app/pages/seller/pages/withdraw/withdraw-route.js
@@ -6,7 +6,7 @@ angular.module('withdraw.route', ['withdraw.controller'])
     .config(['$stateProvider', function($stateProvider) {
         $stateProvider.state('withdraw', {
             url: '/withdraw',
-            templateUrl: '/withdraw.html',
+            templateUrl: 'pages/seller/pages/withdraw/withdraw.html',
             controller: 'WithdrawController'
         })
     }]);
