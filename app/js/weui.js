d655355deb2ce5a56ea264c9dc76fd06fb28e95a
vincent <vincent@kequandian.net>
2016-06-14 10:45:10 +0800

    fix weui.js

--- app/js/weui.js
+++ app/js/weui.js
@@ -1,4 +1,4 @@
-(function(){
+$(function () {
 
     // searchbar
     $('#container').on('focus', '#search_input', function () {
@@ -27,11 +27,11 @@
         $('#search_input').val('');
     })
 
-    //cart settlement action sheet
-    .on('click', '#showAddress', function () {
+        //cart settlement action sheet
+        .on('click', '#showAddress', function () {
 
-            var show_status =  $("#show_address_status").val();
-            if(show_status == "add"){
+            var show_status = $("#show_address_status").val();
+            if (show_status == "add") {
 
                 var add_mask = $('#add_mask');
                 var weui_action_add_sheet = $('#weui_action_add_sheet');
@@ -57,7 +57,7 @@
                     })
                 }
 
-            }else{
+            } else {
 
                 var mask = $('#mask');
                 var weuiActionsheet = $('#weui_actionsheet');
@@ -84,14 +84,11 @@
                 }
             }
 
-    })
-
-
+        })
 
 
         //details action sheet
         .on('click', '.product_info', function () {
-
             var p_value = $('#property_hidden');
 
             var mask = $('#details_mask');
@@ -102,7 +99,7 @@
             });
 
             $('#action_sheet_cancel').on('click', function () {
-                if(p_value.val().length > 0) {
+                if (p_value.val().length > 0) {
                     hideActionSheet(weuiActionsheet, mask);
                 }
             });
@@ -124,82 +121,77 @@
         })
 
 
+        //.on('click', '#showReceivingTime', function () {
+        //    var mask_time = $('#mask_time');
+        //    var weuiActionsheetTime = $('#weui_actionsheet_time');
+        //    weuiActionsheetTime.addClass('weui_actionsheet_toggle');
+        //    mask_time.show().addClass('weui_fade_toggle').one('click', function () {
+        //        hideActionSheet(weuiActionsheetTime, mask_time);
+        //    });
+        //    $('#actionsheet_cancel_time').one('click', function () {
+        //        hideActionSheet(weuiActionsheetTime, mask_time);
+        //    });
+        //    weuiActionsheetTime.unbind('transitionend').unbind('webkitTransitionEnd');
+        //
+        //    function hideActionSheet(weuiActionsheetTime, mask_time) {
+        //        weuiActionsheetTime.removeClass('weui_actionsheet_toggle');
+        //        mask_time.removeClass('weui_fade_toggle');
+        //        weuiActionsheetTime.on('transitionend', function () {
+        //            mask_time.hide();
+        //        }).on('webkitTransitionEnd', function () {
+        //            mask_time.hide();
+        //        })
+        //    }
+        //})
 
+        //
+        //.on('click', '#addressPcd', function () {
+        //    var mask_pcd = $('#mask_pcd');
+        //    var weui_actionsheet_pcd = $('#weui_actionsheet_pcd');
+        //    weui_actionsheet_pcd.addClass('weui_actionsheet_toggle');
+        //    mask_pcd.show().addClass('weui_fade_toggle').one('click', function () {
+        //        hideActionSheet(weui_actionsheet_pcd, mask_pcd);
+        //    });
+        //    $('#actionsheet_cancel_pcd').one('click', function () {
+        //        hideActionSheet(weui_actionsheet_pcd, mask_pcd);
+        //    });
+        //    weui_actionsheet_pcd.unbind('transitionend').unbind('webkitTransitionEnd');
+        //
+        //    function hideActionSheet(weui_actionsheet_pcd, mask_pcd) {
+        //        weui_actionsheet_pcd.removeClass('weui_actionsheet_toggle');
+        //        mask_pcd.removeClass('weui_fade_toggle');
+        //        weui_actionsheet_pcd.on('transitionend', function () {
+        //            mask_pcd.hide();
+        //        }).on('webkitTransitionEnd', function () {
+        //            mask_pcd.hide();
+        //        })
+        //    }
+        //})
 
-
-
-
-    //.on('click', '#showReceivingTime', function () {
-    //    var mask_time = $('#mask_time');
-    //    var weuiActionsheetTime = $('#weui_actionsheet_time');
-    //    weuiActionsheetTime.addClass('weui_actionsheet_toggle');
-    //    mask_time.show().addClass('weui_fade_toggle').one('click', function () {
-    //        hideActionSheet(weuiActionsheetTime, mask_time);
-    //    });
-    //    $('#actionsheet_cancel_time').one('click', function () {
-    //        hideActionSheet(weuiActionsheetTime, mask_time);
-    //    });
-    //    weuiActionsheetTime.unbind('transitionend').unbind('webkitTransitionEnd');
-    //
-    //    function hideActionSheet(weuiActionsheetTime, mask_time) {
-    //        weuiActionsheetTime.removeClass('weui_actionsheet_toggle');
-    //        mask_time.removeClass('weui_fade_toggle');
-    //        weuiActionsheetTime.on('transitionend', function () {
-    //            mask_time.hide();
-    //        }).on('webkitTransitionEnd', function () {
-    //            mask_time.hide();
-    //        })
-    //    }
-    //})
-
-    //
-    //.on('click', '#addressPcd', function () {
-    //    var mask_pcd = $('#mask_pcd');
-    //    var weui_actionsheet_pcd = $('#weui_actionsheet_pcd');
-    //    weui_actionsheet_pcd.addClass('weui_actionsheet_toggle');
-    //    mask_pcd.show().addClass('weui_fade_toggle').one('click', function () {
-    //        hideActionSheet(weui_actionsheet_pcd, mask_pcd);
-    //    });
-    //    $('#actionsheet_cancel_pcd').one('click', function () {
-    //        hideActionSheet(weui_actionsheet_pcd, mask_pcd);
-    //    });
-    //    weui_actionsheet_pcd.unbind('transitionend').unbind('webkitTransitionEnd');
-    //
-    //    function hideActionSheet(weui_actionsheet_pcd, mask_pcd) {
-    //        weui_actionsheet_pcd.removeClass('weui_actionsheet_toggle');
-    //        mask_pcd.removeClass('weui_fade_toggle');
-    //        weui_actionsheet_pcd.on('transitionend', function () {
-    //            mask_pcd.hide();
-    //        }).on('webkitTransitionEnd', function () {
-    //            mask_pcd.hide();
-    //        })
-    //    }
-    //})
-
-    //bar 点击事件
-    .on('click', '.weui_navbar_item', function () {
+        //bar 点击事件
+        .on('click', '.weui_navbar_item', function () {
             $(this).addClass('goods_bar_item_on').siblings('.goods_bar_item_on').removeClass('goods_bar_item_on');
-    })
-    .on('click', '.weui_tabbar_item', function () {
+        })
+        .on('click', '.weui_tabbar_item', function () {
             $(this).addClass('weui_bar_item_on').siblings('.weui_bar_item_on').removeClass('weui_bar_item_on');
             $(this).siblings("a").children('.home_tab_icon').removeClass('home_tab_icon_color');
             $(this).children('.home_tab_icon').addClass('home_tab_icon_color');
-    })
+        })
 
         //地址管理页---新增收件地址
-    .on('click', '#contactMan', function(){
-        $("#city-picker").cityPicker({
-            title: "请选择收货地址"
-        });
-    })
+        .on('click', '#contactMan', function () {
+            $("#city-picker").cityPicker({
+                title: "请选择收货地址"
+            });
+        })
         //结算页---新增收件地址
-        .on('click', '#settl_add_pcd', function(){
+        .on('click', '#settl_add_pcd', function () {
             $("#city-picker").cityPicker({
                 title: "请选择收货地址"
             });
         })
 
-        .on('click', '.click_radio label', function(){
+        .on('click', '.click_radio label', function () {
             var radioId = $(this).attr('name');
             $('.click_radio label').removeAttr('class').addClass('detail_label_radio');
             $(this).attr('class', 'detail_label_radio_checked');
