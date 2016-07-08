7547bc99028a0c471996b86f345f0bcb9e06e017
vincent <vincent@kequandian.net>
2016-07-08 23:27:54 +0800

    squareImg bug

--- app/lib/custom/js/squareImg.js
+++ app/lib/custom/js/squareImg.js
@@ -10,18 +10,23 @@ angular.module('squareImg', [])
             link: function (scope, $el, attrs) {
 
                 var el = $el[0];
-                if( $el[0].width > 0){
-                    $el.css('height',$el[0].width);
-                }else {
-                    $el.css('height', 'auto');
+
+                if( $el[0].clientWidth > 0){
+                    $el.css('height',$el[0].clientWidth);
+                }else{
+                    $el.css('height','auto');
                 }
                 //console.log("squareImg: width?"+el.width+",height?"+el.height);
 
                 scope.$watch(function() {
                     //console.log("watch width?"+ $el[0].width+", height?"+$el[0].height);
-                    return $el[0].width;
-                }, function(value){
-                    $el.css('height', value);
+                    return $el[0].clientWidth;
+                    
+                }, function(value, oldValue){
+
+                    if( ! (el.width> 0 && el.height > 0 && el.width==el.height)) {
+                        $el.css('height', value);
+                    }
                     //console.log("watch onchange()?"+ $el[0].width+", clientHeight?"+$el[0].height);
                 });
 
