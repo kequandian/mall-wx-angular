eb56f0552203afc10f5f5e186327e260b620da61
Your Name <you@examle.com>
2016-07-21 13:16:17 +0800

    commit

--- app/pages/details/details-controller.js
+++ app/pages/details/details-controller.js
@@ -2,7 +2,7 @@ angular.module('details.controller', ['details.service'])
 
     .controller('DetailsController', ['$scope', '$state', '$stateParams', '$rootScope', 'DetailsFty', '$ocLazyLoad',
         function ($scope, $state, $stateParams, $rootScope, DetailsFty, $ocLazyLoad) {
-            
+
             $ocLazyLoad.load('Jquery').then(function () {
                 $ocLazyLoad.load('JqueryWeUI').then(function () {
                     //console.log("homePage:jquery loaded");
