1853a22216177de860f7a3bd2a2eded921cbebf6
Your Name <you@examle.com>
2016-10-08 15:14:33 +0800

    edit details page url

--- app/pages/details/details-route.js
+++ app/pages/details/details-route.js
@@ -1,7 +1,7 @@
 angular.module('details.route', ['bsSwitch'])
     .config(['$stateProvider', function($stateProvider) {
         $stateProvider.state('details', {
-            url: '/details/:productId',
+            url: '/details/:productId?:fallback',
             templateUrl: 'pages/details/details.html',
             controller:'DetailsController',
             resolve: {
