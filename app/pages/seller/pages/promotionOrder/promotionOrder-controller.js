5524fb3d399b2de2a6c2cf3da771b8e7424e5808
Your Name <you@examle.com>
2016-11-14 13:43:06 +0800

    promotion order

--- app/pages/seller/pages/promotionOrder/promotionOrder-controller.js
+++ app/pages/seller/pages/promotionOrder/promotionOrder-controller.js
@@ -10,45 +10,6 @@ angular.module('promotionOrder.controller', ['promotionOrder.service', 'seller.s
 
             $scope.point_rate = PointRate.rate;
 
-            /*$scope.pro_order_groups = [
-             {
-             name: "最近更新",
-             items: [{
-             finish:'测试1'
-             }],
-             show: false
-             },{
-             name: "一周内",
-             items: [{
-             finish:'测试3'
-             }],
-             show: false
-             },{
-             name: "一个月内",
-             items: [{
-             finish:'测试5'
-             }],
-             show: false
-             },{
-             name: "更早",
-             items: [{
-             finish:'测试7'
-             }],
-             show: false
-             }
-             ];*/
-
-            /*
-             * if given group is the selected group, deselect it
-             * else, select the given group
-             */
-            /*$scope.toggleGroup = function(group) {
-             group.show = !group.show;
-             };
-             $scope.isGroupShown = function(group) {
-             return group.show;
-             };*/
-
             // 年月
             $scope.yearDefault = getDefaultYears();
 
@@ -68,7 +29,7 @@ angular.module('promotionOrder.controller', ['promotionOrder.service', 'seller.s
             function getPromotionOrders(start_date, end_date) {
                 PromotionOrderFty.promotionOrdersService(start_date, end_date)
                     .then(function (json) {
-                        // alert(angular.toJson(json));
+                         console.log(angular.toJson(json));
                         if (json.status_code == 0) {
                             $scope.rewards = json.data;
                             //console.log(angular.toJson($scope.rewards));
