f98693251d461ec18f3ce7af11d65f25390da890
Your Name <you@examle.com>
2017-06-23 15:16:11 +0800

    测试需知api 2

--- app/pages/seller/pages/offLineShop/offLineShop-route.js
+++ app/pages/seller/pages/offLineShop/offLineShop-route.js
@@ -154,7 +154,7 @@ angular.module('offLineShop.route',[])
              * */
             .state('applynotice', {
                 url:'/applynotice/:recommenderId/:typeStatus/:applyStatus',
-                templateUrl: 'seller/sellerAuthorization/apply-notice.html',
+                templateUrl: 'seller/sellerAuthorization/applynotice.html',
                 controller:'ApplynoticeController',
                 resolve: {
                     loadData: ['$ocLazyLoad', function ($ocLazyLoad) {
